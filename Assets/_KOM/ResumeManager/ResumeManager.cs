0967758c743505f2e6ba337f4dbe007c70610883
Mireene-Unity <dhalsghkdxld@gmail.com>
2025-04-22 21:17:58 +0900

    Feat_ResumeManager

---
+++ Assets/_KOM/ResumeManager/ResumeManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+
+public struct EmployeeData
+{
+    int designSkil;
+    int devSkill;
+    int artSkill;
+
+    int designExp;
+    int devExp;
+    int artExp;
+
+    float stress;
+    int joinDay;
+    int salary;
+}
+
+public class ResumeManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
