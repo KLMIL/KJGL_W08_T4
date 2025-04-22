0c4a9ef14583d1f2170f8391f6e44d5a4fd47e63
Dongyeong Kim <withdongyeong@gmail.com>
2025-04-22 21:22:46 +0900

    updating project Manager

---
+++ Assets/_KDY/Project/ProjectManager.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectManager : MonoBehaviour
+{
+    private List<Project> _currentProjects;
+    private List<Employee> 
+    
+    // Getter
+    public List<Project> CurrentProjects => _currentProjects;
+    
+    public Project MakeProject()
+    {
+        
+    }
+    
+    
+    
+}
