5f7ada20adba1348f82ecfc92590326e5a1ddf52
KLMIL <kanoline@naver.com>
2025-04-28 11:44:00 +0900

    [balance] 능력 요구량, 작업량 밸런스 조정

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -14,7 +14,7 @@ public class GameManager : MonoBehaviour
     [SerializeField] private string _companyName;
     private int _roomLevel = 0;
 
-    private int _companyFunds = 3000; // 초기 자산 예시
+    private int _companyFunds = 300000; // 초기 자산 예시
     private const int MaxEmployeeCount = 40;
 
     public int CompanyFunds => _companyFunds;
